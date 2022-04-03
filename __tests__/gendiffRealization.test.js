1210552ea8d66059e5a926c2cea495cc36ef6c1c
alexander <emyleigh@inbox.ru>
2022-04-03 15:48:18 +0300

    add jest to env

--- __tests__/gendiffRealization.test.js
+++ __tests__/gendiffRealization.test.js
@@ -1,18 +1,16 @@
-import genDiff from "../gendiffRealization";
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import { readFileSync } from "fs";
-import { join } from "path";
+import { dirname, join } from 'path';
+import { readFileSync } from 'fs';
+import genDiff from '../gendiffRealization.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 test('gediff', () => {
-    const expected = readFileSync(getFixturePath('results'), 'utf-8')
+  const expected = readFileSync(getFixturePath('results'), 'utf-8');
 
-    const result = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))
-    expect(result).toEqual(expected);
-})
+  const result = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+  expect(result).toEqual(expected);
+});

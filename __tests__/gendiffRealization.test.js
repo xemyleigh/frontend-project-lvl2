461e111ed3c5e92704f71c52d4956ce8ea2b1eb3
alexander <emyleigh@inbox.ru>
2022-04-09 10:46:53 +0300

    fix tests& & main

--- __tests__/gendiffRealization.test.js
+++ __tests__/gendiffRealization.test.js
@@ -8,20 +8,6 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 
-test('gediff json test', () => {
-  const expected = readFileSync(getFixturePath('results'), 'utf-8');
-
-  const result = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  expect(result).toEqual(expected);
-});
-
-test('gediff yaml test', () => {
-  const expected = readFileSync(getFixturePath('results'), 'utf-8');
-
-  const result = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
-  expect(result).toEqual(expected);
-});
-
 test('gediff recurtion json test', () => {
   const expected = readFileSync(getFixturePath('recurtionResult'), 'utf-8');
 
